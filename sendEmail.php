2b9f5b51eb57628eb76d3153978435899c304eaf
guoclara <48026886+guoclara@users.noreply.github.com>
2020-06-15 21:03:42 -0700

    updated form submission
    trying new php code

--- sendEmail.php
+++ sendEmail.php
@@ -1,42 +1,50 @@
 <?php
-
-// if (empty($_POST('message'))) {
-//     echo "The message body is empty";
-//     exit;
-// }
-
-// if (empty($_POST('name'))) {
-//     echo "Please fill out the name field";
-//     exit;
-// }
-
-// if (empty($_POST('reason'))) {
-//     echo "Please select a reason for comtact";
-//     exit;
-// }
-
-// if (empty($_POST('email'))) {
-//     echo "Please fill out the email field";
-//     exit;
-// }
-
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $subject = $_POST['reason'];
-    $body = $_POST['message'];
-    $email = $_POST['email'];
-    $headers = "Reply-To: $email \n";
-    // $csa = "thebrowncsa@gmail.com";
-    $csa = "thebrowncsa@gmail.com";
-
-    mail($csa, "New Form Submission: $subject", "Name: $name \n\n Message: \n $body", $headers);
-
-    if (isset($_POST['checkbox'])) {
-        $subject2 = "Copy of Your Form Submission";
-        $body2 = "Here is a copy of your message: \n$body";
-        $headers2 = "From: $csa";
-        mail($email, $subject2, $body2, $headers2);
+ 
+if($_POST) {
+    $name = "";
+    $email = "";
+    $reasonforcontact = "";
+    $message = "";
+     
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     }
+     
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+     
+    if($reasonforcontact == "Option 1") {
+        $reasonforcontact = "Membership";
+    } 
+    else if($reasonforcontact == "Option 2") {
+        $reasonforcontact = "Suggestions";
+    } 
+    else if($reasonforcontact == "Event Inquiries/Partnerships") {
+        $reasonforcontact = "Alumni";
+    } 
+    else if($reasonforcontact == "Option 4") {
+        $reasonforcontact = "Other";
+    } 
+     
+    if(isset($_POST['message'])) {
+        $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+    }
+     
+    #change to fintech@brown.edu
+    $recipient = "clara_guo@brown.edu";
+     
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+​
+    mail($recipient, "Contact Form Response", $message);
+    
+    
+     
+} else {
+    echo '<p>Something went wrong</p>';
 }
-
+ 
 ?>
