ca8039b846faaa6dcb90a789a21134c2a0a6c65c
guoclara <48026886+guoclara@users.noreply.github.com>
2020-07-05 15:14:20 -0700

    comments added to css and js
    commented custom and navigation

--- navigation.js
+++ navigation.js
@@ -1,3 +1,6 @@
+// toggles the nav links between visible and not visible
+// adds the fade in animation for links
+// toggles the burger between up arrow and down arrow when pointer hovers over it
 function navSlide(){
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
@@ -15,12 +18,17 @@ function navSlide(){
   });
 }
 
+//turns top left home button to turn yellow on hover
 function hover(element) {
   element.setAttribute('src', "images/csaLogoYellow.png");
   const text = document.querySelector(".homeSection");
   text.style.color = "#FFE12B";
 }
 
+//turns top left home button to return to white or red depending on if
+//nav menu is active or not
+
+//only used for home page
 function unhoverHomePage(element) {
   const nav = document.querySelector(".nav-links");
   const text = document.querySelector(".homeSection");
@@ -33,6 +41,7 @@ function unhoverHomePage(element) {
   }
 }
 
+//turns top left home buttom back to red for all pages except index
 function unhoverNormal(element) {
   element.setAttribute('src', "images/csaLogo.png");
   document.querySelector(".homeSection").style.color = "#CF2127";
@@ -46,23 +55,27 @@ function dropdownToggleScroll() {
   }
 }
 
+//toggles top left home button color between red and white
+//red when the nav menu is activated and white when not
 function changeColor() {
   const logo = document.getElementById('homeIcon');
   const text = document.querySelector(".homeSection");
 
   if (logo.getAttribute('src')==="images/csaLogoWhite.png"){
-    setTimeout(delayLogoChange = () => { logo.setAttribute('src', "images/csaLogo.png");}, 600);
-    setTimeout(delayTextChange = () => { text.style.color = "#CF2127";}, 600);
+    setTimeout(delayLogoChange = () => { logo.setAttribute('src', "images/csaLogo.png");}, 625);
+    setTimeout(delayTextChange = () => { text.style.color = "#CF2127";}, 625);
   } else {
     logo.setAttribute('src', "images/csaLogoWhite.png");
     text.style.color = "white";
   }
 }
 
+//activates navSlide function when burger clicked
 document.addEventListener("DOMContentLoaded", function(){
   $(".burger").click(navSlide)
 })
 
+//activates changeColor when on landing page
 document.addEventListener("DOMContentLoaded", function(){
   if(document.getElementById("home")) {
     $(".burger").click(changeColor)
