78e89025431fa4a65b819281650e31cdf52fb026
guoclara <48026886+guoclara@users.noreply.github.com>
2020-07-08 21:50:49 -0700

    fixed landing page issue
    modified displayLanding in navigation.js to fix text shifting of landing page

--- navigation.js
+++ navigation.js
@@ -37,7 +37,7 @@ function displayMajority(){
 
 //gets rid of page behind the dropdown when nav is active on home page, fixes overflow
 function displayLanding() {
-  const homeContainer = document.querySelector(".homePageContainer");
+  const homeContainer = document.querySelector(".home");
   if (homeContainer.style.display === "none") {
     homeContainer.style.display = "block";
   } else {
